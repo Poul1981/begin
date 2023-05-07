2f58833258401bcde3d2f54c6c4baaeccf01e9f5
Pavel Kursenko <pavelkursenko@yandex.ru>
2023-05-07 11:21:49 +0300

    next

--- handler.php
+++ handler.php
@@ -1,4 +1,9 @@
 <?php
-echo 'hande home';
-echo 'dffffefe';
-echo time();
+//echo 'hande home';
+//echo 'dffffefe';
+//print_r($_GET);
+//echo time(); echo '<br>';
+//print_r($_GET);
+if (isset($_GET['text'])){
+    echo $_GET['text'];
+}
--- js.js
+++ js.js
@@ -1,3 +1,31 @@
 //alert('hello')
 
-console.log('console');
+//console.log('console');
+async function resp(name){
+    let str = '';
+    let otvet = await fetch('handler.php?name=' + name);
+    let ot1 = await otvet.text();
+    //console.log(ot1);
+}
+
+//resp('vivat');
+
+
+
+$(function (){
+    //console.log('clik')
+   $('#btn_1').click(()=>{
+       alert('clicl');
+   });
+    $('#btn_2').on('click', getText);
+
+    async function getText() {
+        let text = $('#txt_1').val();
+        //console.log(text);
+        let res = await fetch('handler.php?text=' + text);
+        let res1 = await res.text();
+        console.log(res1);
+        $('.text').append(res1);
+    }
+
+});
